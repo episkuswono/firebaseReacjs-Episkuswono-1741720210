814396d725ff1768ce01a3bf0e77e12a9955db1f
episkuswono <episkuswono@gmail.com>
2020-04-16 16:47:11 +0700

    init

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyCr8rBWGUa45nU7l2agN_-jmJ_XegZeiyY",
+    authDomain: "react-5903b.firebaseapp.com",
+    databaseURL: "https://react-5903b.firebaseio.com",
+    projectId: "react-5903b",
+    storageBucket: "react-5903b.appspot.com",
+    messagingSenderId: "141745801733",
+    appId: "1:141745801733:web:a3b7a79078b993d0576a6c",
+    measurementId: "G-5NPBRCLPHQ"
+}
