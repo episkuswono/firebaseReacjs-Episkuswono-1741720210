b531d32825f2c6933d170f0f6ed86c93d5511af5
episkuswono <episkuswono@gmail.com>
2020-04-16 17:28:43 +0700

    service

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,10 +1,10 @@
 export default {
-    apiKey: "AIzaSyCr8rBWGUa45nU7l2agN_-jmJ_XegZeiyY",
-    authDomain: "react-5903b.firebaseapp.com",
-    databaseURL: "https://react-5903b.firebaseio.com",
-    projectId: "react-5903b",
-    storageBucket: "react-5903b.appspot.com",
-    messagingSenderId: "141745801733",
-    appId: "1:141745801733:web:a3b7a79078b993d0576a6c",
-    measurementId: "G-5NPBRCLPHQ"
+    apiKey: "AIzaSyAgfS2lgR67BXB-ijnh5O3ejSOFXIhdUAE",
+    authDomain: "react-e6fa1.firebaseapp.com",
+    databaseURL: "https://react-e6fa1.firebaseio.com",
+    projectId: "react-e6fa1",
+    storageBucket: "react-e6fa1.appspot.com",
+    messagingSenderId: "309172088771",
+    appId: "1:309172088771:web:a34bca30e829ab73df1776",
+    measurementId: "G-2DBRGCJKN9"
 }
